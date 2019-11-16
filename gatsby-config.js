65dc60d9f25a131bfb4635edecdb10382b208466
Souleymane <souleymane11dembele@gmail.com>
2019-11-15 20:09:46 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require('dotenv').config({
 module.exports = {
   siteMetadata: {
     title: 'Souleymane Dembele',
-    siteUrl: 'https://souleymanedembele.com',
+    siteUrl: 'https://www.souleymanedembele.com',
     menuLinks: [
       {
         name: 'Blog',
