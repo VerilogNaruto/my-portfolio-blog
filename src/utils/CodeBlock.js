6472c997ca1262ea1c4efb3bc0ef203685b4d584
naruto958 <souleymane11dembele@gmail.com>
2019-12-07 02:50:08 -0800

    Hightlight

--- src/utils/CodeBlock.js
+++ src/utils/CodeBlock.js
@@ -11,7 +11,7 @@ export default ({ children, className }) => {
   return (
     <Highlight {...defaultProps} code={children.trim()} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style}>
+        <pre className={className} style={{ ...style, padding: '20px' }}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
